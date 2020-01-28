756ba954e1fe7504d5829994f6b2772f32c85238
techniponi <wincam97@gmail.com>
2020-01-28 02:20:50 -0600

    implement user id and interval loop

--- zero-client.js
+++ zero-client.js
@@ -1,6 +1,8 @@
 const request = require("request");
 
+// command arguments
 let url = process.argv[2];
+let id = process.argv[3];
 
 // bitch at the user to specify a url
 if (!url) {
@@ -8,20 +10,26 @@ if (!url) {
   return 1;
 }
 
+// if the user does not specify an id, set to 'test'
+if (!id) {
+  id = "test";
+}
+
 // automatically insert http://
 if (!url.startsWith("http://")) {
   url = "http://" + url;
 }
 
-const options = {
+// URL query options
+let query = {
   url: url,
   qs: {
-    test: "test"
+    id: id // this identifies which device is connecting
   }
 };
 
 // create request
-let createRequest = function() {
+let createRequest = function(options) {
   console.log("Making request with options:\n");
   console.log(options);
   request(options, (err, res, body) => {
@@ -32,4 +40,10 @@ let createRequest = function() {
   });
 };
 
-createRequest();
+// initial ping before we loop
+createRequest(query);
+
+// loop every 60 sec
+setInterval(function() {
+  createRequest(query);
+}, 60000);
