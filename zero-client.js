ce37b607049f04e90b6dbc1d2b4b578270387f8e
techniponi <wincam97@gmail.com>
2020-01-28 01:34:56 -0600

    automatically insert http:// if not present

--- zero-client.js
+++ zero-client.js
@@ -1,24 +1,35 @@
-const request = require('request');
+const request = require("request");
+
+let url = process.argv[2];
 
 // bitch at the user to specify a url
-if (!process.argv[2]) {
-    console.log("You must specify a URL!");
-    return 1;
+if (!url) {
+  console.log("You must specify a URL!");
+  return 1;
+}
+
+// automatically insert http://
+if (!url.startsWith("http://")) {
+  url = "http://" + url;
 }
 
 const options = {
-    url: process.argv[2],
-    qs: {
-    	test: "test"
-    }
-}
+  url: url,
+  qs: {
+    test: "test"
+  }
+};
 
 // create request
 let createRequest = function() {
-    request(options, (err, res, body) => {
-        if (err) { return console.log(err); }
-        console.log(body);
-    });
-}
+  console.log("Making request with options:\n");
+  console.log(options);
+  request(options, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("\nResponse body:\n" + body);
+  });
+};
 
 createRequest();
