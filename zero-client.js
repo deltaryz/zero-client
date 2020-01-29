47f6cb78fa341ba8a1406028d8a5476b0e14b2e0
techniponi <wincam97@gmail.com>
2020-01-29 00:46:47 -0600

    implement shelljs and check for mpstat

--- zero-client.js
+++ zero-client.js
@@ -1,4 +1,11 @@
-const request = require("request");
+const request = require("request"); // we use this to report our data to the server
+const shell = require("shelljs"); // we use this to easily get info from the system
+
+// check and make sure we have our dependencies
+if (!shell.which("mpstat")) {
+  console.log("mpstat not found - you need the sysstat package installed");
+  process.exit(1);
+}
 
 // command arguments
 let url = process.argv[2];
@@ -20,19 +27,27 @@ if (!url.startsWith("http://")) {
   url = "http://" + url;
 }
 
-// URL query options
-// TODO: add some more shit!
-let query = {
-  url: url,
-  qs: {
-    id: id // this identifies which device is connecting
-  }
-};
-
 // create request
-let createRequest = function(options) {
-  console.log("Making request with options: ", options);
-  request(options, (err, _res, body) => {
+let createRequest = function() {
+  // URL query options
+  // these will be changed with every 'tick'
+  // TODO: add some more shit!
+
+  // gather variables
+  let cpustring = shell.exec("mpstat | grep 'all'");
+  let cpu = 100.0 - parseFloat(cpustring.substr(cpustring.length - 7));
+  console.log("Current CPU usage: " + cpu);
+
+  let query = {
+    url: url,
+    qs: {
+      id: id, // this identifies which device is connecting
+      cpu: cpu
+    }
+  };
+
+  console.log("Making request with options: ", query);
+  request(query, (err, _res, body) => {
     if (err) {
       console.log("Error making request:\n", err);
       process.exit(1); // exit program since the thing failed
@@ -54,11 +69,8 @@ let createRequest = function(options) {
 };
 
 // initial ping before we loop
-createRequest(query);
-
-// loop every 60 sec
+createRequest();
+// tick every 60 sec
 setInterval(function() {
-  // TODO: crunch some data
-
-  createRequest(query);
+  createRequest();
 }, 60000);
